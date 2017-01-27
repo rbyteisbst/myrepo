48daeb28b290b70e38be6fba73561b20d4478bde
rbyteisbst <wlamont@gmail.com>
2017-01-26 22:44:41 -0500

    drought data

--- code/map.R
+++ code/map.R
@@ -1,7 +1,7 @@
 #watercolor map
 
 library(tidyverse)
-
+library(lubridate)
 test <- read_csv("~/Downloads/test - Sheet1-2.csv")
 
 library(ggmap)
@@ -20,7 +20,43 @@ ggmap(map) +
   theme(text = element_text(size=14),
         legend.position = "none")
 
-devtools::install_github("dkahle/ggmap")
-https://www.google.com/maps/place/44°28'33.0%22N+73°12'43.0%22W/@44.475833,-73.211944,12z/data=!4m5!3m4!1s0x0:0x0!8m2!3d44.475833!4d-73.211944?hl=en
+#devtools::install_github("dkahle/ggmap")
+#https://www.google.com/maps/place/44°28'33.0%22N+73°12'43.0%22W/@44.475833,-73.211944,12z/data=!4m5!3m4!1s0x0:0x0!8m2!3d44.475833!4d-73.211944?hl=en
+
+drought <- read_csv("~/Downloads/test - Sheet1-3.csv")
+drought_desc <- read_csv("~/Downloads/test - Sheet1-4.csv")
+
+library(scales)
+
+summary(drought) 
+
+drought %>% 
+  gather(key=drought_type,value=percent_applicable,-Week) %>% 
+  inner_join(drought_desc) %>% 
+  transmute(Year = Week,percent_applicable = percent_applicable/100,description) %>% 
+  #filter(year(Year)==2001) %>% 
+ # filter(drought_type %in% c('D0-D4','D1-D4','D2-D4')  ) %>% 
+  filter(!grepl("E", description)) %>% 
+#grep("E",drought_desc$description)
+  ggplot(aes(Year,percent_applicable,color=description))+
+  geom_point()+
+  #geom_line()+
+  geom_smooth(se=F)+
+  scale_y_continuous(label=percent)+
+  facet_wrap(~description)
 
 
+drought %>% 
+  gather(key=drought_type,value=percent_applicable,-Week) %>% 
+  inner_join(drought_desc) %>% 
+  transmute(Year = year(Week),percent_applicable = percent_applicable/100,description) %>% 
+  #filter(year(Year)==2001) %>% 
+  # filter(drought_type %in% c('D0-D4','D1-D4','D2-D4')  ) %>% 
+  filter(!grepl("E", description) & percent_applicable > 0) %>% 
+  group_by(Year,description) %>% 
+  summarize(non_zero_weeks = n()) %>% 
+  #grep("E",drought_desc$description)
+  ggplot(aes(Year,non_zero_weeks,color=description))+
+  geom_point()+
+  geom_line(alpha=.1)+
+  scale_y_continuous(label=percent)
